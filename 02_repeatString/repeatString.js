28204d19c2d2a5418bebf22570b5135321504d9f
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-05-25 22:23:15 +0530

    Do exercise 01 and 02 of javascript

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let ans = '';
+    if(times == 0){
+            return '';
+    }else if(times < 0 ) {
+            return "ERROR"
+    }
+    while (times) { 
+        ans = ans + string
+        times--;
+    }
+    return ans;
 };
 
 // Do not edit below this line
