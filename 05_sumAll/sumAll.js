9ad42af82c549fc0c4e677236974cfd73bdadd71
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-05-25 23:03:50 +0530

    Complete exercise 05 of js odin project

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sumAll = 0
+    if ((start >= 0 && end >= 0) && (typeof start == "number" && typeof end =="number")){
 
+        if(start <= end){
+            for(let i = start; i <= end; i++ ){
+                sumAll += i;
+            }
+        } else if (end <= start){
+            for(let i = start; i >= end; i-- ){
+                sumAll += i;
+            }
+        }
+        return sumAll
+    } else {
+        return "ERROR"
+    }
+        
 };
 
 // Do not edit below this line
