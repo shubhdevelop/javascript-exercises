506916630696234f5fdfd6428d18bfe5ac60e351
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-05-25 22:45:20 +0530

    complete 03 excercise of js odin project

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = '';
+    if (! string == ''){
+        for (let i = 0 ; i < string.length; i++){
+            reverseString = reverseString + string[string.length-i -1];
+        } 
+        return reverseString
+    }else{
+        return '';
+    }
 };
+    reverseString("hello there!")
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
