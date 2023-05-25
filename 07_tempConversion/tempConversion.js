def4d6de5bb0b914d609279e5bd1766695c33be2
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-05-25 23:17:20 +0530

    Complete exercise 07 of js odin project

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celisus = (f - 32) * 5/9
+  return +`${celisus.toFixed(1)}`
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let Fahrenheit = (c * (9/5)) + 32 
+    return +`${Fahrenheit.toFixed(1)}`
 };
+console.log(
+
+
+  convertToFahrenheit(32),
+  convertToCelsius(100)
+);
 
 // Do not edit below this line
 module.exports = {
